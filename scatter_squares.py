3e9fac76fe88a9c0df06305134fe36b1cb81cd39
muziriyueliming <muziriyuelliming@gmail.com>
2021-05-21 23:07:27 +0800

    自定义曲线颜色。

--- scatter_squares.py
+++ scatter_squares.py
@@ -7,7 +7,8 @@ plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
 x_values = range(1, 1001)
 y_values = [x**2 for x in x_values]
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=10)
+# ax.scatter(x_values, y_values, c='red', s=10)
+ax.scatter(x_values, y_values, c=(0, 0.8, 0), s=10)
 # 设置图标标题并给坐标轴加上标签
 ax.set_title("平方数", fontsize=24)
 ax.set_xlabel("值", fontsize=14)
