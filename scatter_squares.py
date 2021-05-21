39d4cb6c3e3474b0190ed34ba79ee600521c0a3d
muziriyueliming <muziriyuelliming@gmail.com>
2021-05-21 23:58:26 +0800

    使用颜色映射，和自动保存图标.

--- scatter_squares.py
+++ scatter_squares.py
@@ -8,7 +8,10 @@ x_values = range(1, 1001)
 y_values = [x**2 for x in x_values]
 fig, ax = plt.subplots()
 # ax.scatter(x_values, y_values, c='red', s=10)
-ax.scatter(x_values, y_values, c=(0, 0.8, 0), s=10)
+# ax.scatter(x_values, y_values, c=(0, 0.8, 0), s=10)
+# ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
+# https://matplotlib.org/stable/gallery/color/colormap_reference.html#sphx-glr-gallery-color-colormap-reference-py 参考颜色的网站
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.spring, s=10)
 # 设置图标标题并给坐标轴加上标签
 ax.set_title("平方数", fontsize=24)
 ax.set_xlabel("值", fontsize=14)
@@ -20,4 +23,5 @@ ax.tick_params(axis='both', which='major', labelsize=14)
 # 设置每个坐标轴的取值范围
 ax.axis([0, 1100, 0, 1100000])
 
-plt.show()
+# plt.show()
+plt.savefig('squares_plot.png', bbox_inches='tight')
