a09f48897fe2d52c45303e9a1f26cd6d03d3cc50
muziriyueliming <muziriyuelliming@gmail.com>
2021-05-21 23:01:09 +0800

    自动计算数据。

--- scatter_squares.py
+++ scatter_squares.py
@@ -4,10 +4,10 @@ plt.style.use('seaborn')
 plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
 plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=100)
+ax.scatter(x_values, y_values, s=10)
 # 设置图标标题并给坐标轴加上标签
 ax.set_title("平方数", fontsize=24)
 ax.set_xlabel("值", fontsize=14)
@@ -15,5 +15,8 @@ ax.set_ylabel("值的平方", fontsize=14)
 
 # 设置刻度标记的大小--set-upstream 15SCSJ dev
 ax.tick_params(axis='both', which='major', labelsize=14)
+
+# 设置每个坐标轴的取值范围
+ax.axis([0, 1100, 0, 1100000])
 
 plt.show()
