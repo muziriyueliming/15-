13205f3b056d1bde4b41cb7c6ae4adb96eb1643c
muziriyueliming <muziriyuelliming@gmail.com>
2021-05-22 12:15:04 +0800

    给点着色。

--- rw_visual.py
+++ rw_visual.py
@@ -11,7 +11,10 @@ while True:
     #将所有点都绘制出来
     plt.style.use('classic')
     fig, ax = plt.subplots()
-    ax.scatter(rw.x_values, rw.y_values, s =15)
+    point_numbers = range(rw.num_points)
+
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
+        edgecolors='none', s =15)
     plt.show()
 
     keep_running = input("Make another walk? (y/n): ")
