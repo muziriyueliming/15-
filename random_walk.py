91739682f8d20932b685839e48ddb9b6f3a0dfe9
muziriyueliming <muziriyuelliming@gmail.com>
2021-05-22 11:06:08 +0800

    选择方向，行走距离，下一个点的位置

--- random_walk.py
+++ random_walk.py
@@ -9,3 +9,33 @@ class Random:
         # 所有随机漫步都始于(0, 0)
         self.x_values = [0]
         self.y_values = [0]
+
+    def fill_wallk(self):
+        """计算漫步包含的所有点就。"""
+
+        # 不断漫步知道列表到达指定的长度。
+        while len(x_values) < self.num_points:
+            
+            # 决定前进方向及沿这个方向前进的距离
+            x_direction = choice([1, -1])
+
+            x_distance = choice([0, 1, 2, 3, 4])
+            x_step = x_direction * x_distance
+
+            y_direction = choice([1, -1])
+
+            y_distance = choice([0, 1, 2, 3, 4])
+            y_step = y_direction * y_distance
+
+            # 拒绝原地踏步
+            if x_step ==0 and y_step == 0:
+                continue
+
+            # 计算下一个点的x值和y值
+            x = self.x_values[-1] + x_step
+            y = self.y_values[-1] + y_step
+
+            self.x_values.append(x)
+            self.y_values.append(y)
+
+
